da6891e2d2aca32e45c3220def54ee4f5fa41cb5
Raphael Barros <127904171+FaelB-coder@users.noreply.github.com>
2023-03-16 20:11:57 -0300

    Add files via upload

---
+++ CalcularArea.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calcular.area;
+
+import static java.time.Clock.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 20231tpmi0465
+ */
+public class CalcularArea {
+  
+            
+    public static void main(String[] args) {
+        
+       double ladoq;
+       double areaq;
+       ladoq = 5;
+       areaq = ladoq * ladoq;
+       
+       double baset;
+       double alturat;
+       double areat;
+       baset = 4;
+       alturat = 3;
+       areat = baset * alturat/2;
+       
+       double diametrol;
+       double pil;
+       double raiol;
+       pil = 3.1415;
+       raiol = 3;
+       diametrol = 2*pil * raiol;
+       
+       
+       Scanner in = new Scanner (System.in);
+       in.next();
+               
+        System.out.println("digite o lado maior do ret");
+        double ladoMaior - in.next double;
+       
+    }
+    
+}
